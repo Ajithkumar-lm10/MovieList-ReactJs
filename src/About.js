25f3e757efd9d75afa886a9c58c7f5ae54529998
Ajithkumarlm10 <ajithkumarak6649@gmail.com>
2023-05-20 22:47:19 +0530

    Fixed code

--- src/About.js
+++ src/About.js
@@ -6,14 +6,13 @@ import TwitterIcon from "@mui/icons-material/Twitter";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import { useHistory } from "react-router-dom";
-import { Link } from "react-router-dom";
 
 function About() {
   const history = useHistory();
   return (
     <div className="center gap">
       <a
-        target={"_blank"}
+        target="{_blank}"
         href="https://www.linkedin.com/in/prithiviraj-mern-fullstack"
       >
         <Fab color="primary" aria-label="add">
@@ -21,29 +20,29 @@ function About() {
         </Fab>
       </a>
         <a
-          target={"_blank"}
-          href="https://github.com/Prithiv11">
+          target="{_blank}"
+          href="https://github.com/Ajithkumar-lm10">
         <Fab color="error" aria-label="add">
           <GitHubIcon/>
         </Fab>
       </a>
       <a
-        target={"_blank"}
-        href="https://twitter.com/prithiviraj624"
+        target="{_blank}"
+        href="https://twitter.com/messi_blinder"
       >
       <Fab color="warning" aria-label="add">
         <TwitterIcon />
       </Fab></a>
       <a
-        target={"_blank"}
-        href="https://www.instagram.com/prithiv_here/"
+        target="{_blank}"
+        href="https://www.instagram.com/ajith_lm10/"
       >
       <Fab color="secondary" aria-label="add">
         <InstagramIcon />
       </Fab></a>
       <a
-        target={"_blank"}
-        href="https://www.facebook.com/prithiviraj.thala.7">
+        target="{_blank}"
+        href="https://www.facebook.com/ajithkumar">
         <Fab color="primary" aria-label="add">
           <FacebookIcon />
         </Fab>
